eceb17441ff1b390fa90e3df6411f5585193a2e5
Shilpi <svvar31@gmail.com>
2023-02-10 23:50:39 +0530

    Second Submit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,93 +1,3 @@
-//package com.driver;
-//
-//public class Pizza {
-//
-//    private int price;
-//    private Boolean isVeg;
-//    private String bill;
-//
-//    private int toppings;
-//    private int cheese;
-//    // To check if cheese already added :
-//    boolean isCheeseAdded;
-//    // To check if extra topping already added :
-//    boolean isExtraToppingAdded;
-//    // To check if bill already created :
-//    boolean isBillCreated;
-//    boolean isTakeAway;
-//
-//    // Cannot change the number of parameters :
-//    public Pizza(Boolean isVeg){
-//        this.isVeg = isVeg;
-//        // your code goes here
-//        // For Veg
-//        if(isVeg) {
-//            this.price = 300;
-//            this.toppings = 70;
-//        } else { // For Non-Veg
-//            this.price = 400;
-//            this.toppings = 120;
-//        }
-//
-//        // Optional
-//        /*  this.isCheeseAdded = false;
-//            this.isExtraToppingAdded = false;
-//            this.isBillCreated = false;
-//            this.isTakeAway = false;
-//         */
-//
-//        this.cheese = 80;
-//        // Initializing the Bill :
-//        this.bill = "Base Price of The Pizza: " + this.price + "\n";
-//    }
-//
-//    public int getPrice(){
-//        return this.price;
-//    }
-//
-//    public void addExtraCheese(){
-//        // your code goes here
-//        if(!isCheeseAdded) {
-//            this.price += this.cheese;
-//            this.isCheeseAdded = true;
-//        }
-//    }
-//
-//    public void addExtraToppings(){
-//        // your code goes here
-//        if(!isExtraToppingAdded) {
-//            this.price += this.toppings;
-//            this.isExtraToppingAdded = true;
-//        }
-//    }
-//
-//    public void addTakeaway(){
-//        // your code goes here
-//        if(!isTakeAway) {
-//            this.price += 20;
-//            this.isTakeAway = true;
-//        }
-//    }
-//
-//    public String getBill(){
-//        // your code goes here
-//        if(!isBillCreated) {
-//            if(isCheeseAdded) {
-//                this.bill += "Extra Cheese Added: " + this.cheese + "\n";
-//            }
-//            if(isExtraToppingAdded) {
-//                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
-//            }
-//            if(isTakeAway) {
-//                this.bill += "Paperbag Added: 20" + "\n";
-//            }
-//            this.bill += "Total Price: " + this.price + "\n";
-//            this.isBillCreated = true;
-//        }
-//        return this.bill;
-//    }
-//}
-
 package com.driver;
 
 public class Pizza {
@@ -95,29 +5,42 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+
+    private int toppings;
     private int cheese;
-    private int topping;
+    // To check if cheese already added :
     boolean isCheeseAdded;
-    boolean isToppingAdded;
-    boolean isbagAdded;
-    boolean isBillGenerated;
+    // To check if extra topping already added :
+    boolean isExtraToppingAdded;
+    // To check if bill already created :
+    boolean isBillCreated;
+    boolean isTakeAway;
+
+    // Cannot change the number of parameters :
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-        this.isbagAdded = false;
-        this.isCheeseAdded = false;
-        this.isBillGenerated = false;
-        this.isToppingAdded = false;
-        this.bill = "";
-        if(isVeg){
+        // For Veg
+        if(isVeg) {
             this.price = 300;
-            this.topping = 70;
-        }else{
+            this.toppings = 70;
+        } else {
+            // For Non-Veg
             this.price = 400;
-            this.topping =  120;
+            this.toppings = 120;
         }
+
+        // Optional
+        /*  this.isCheeseAdded = false;
+            this.isExtraToppingAdded = false;
+            this.isBillCreated = false;
+            this.isTakeAway = false;
+         */
+
         this.cheese = 80;
-        this.bill = "Base Price Of The Pizza: "+this.price+"\n";
+        // Initializing the Bill :
+//        this.bill = "";
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
     public int getPrice(){
@@ -126,8 +49,7 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        // same price for both veg and non-veg pizza
-        if(!isCheeseAdded){
+        if(!isCheeseAdded) {
             this.price += this.cheese;
             this.isCheeseAdded = true;
         }
@@ -135,37 +57,33 @@ public class Pizza {
 
     public void addExtraToppings(){
         // your code goes here
-        if(!isToppingAdded){
-            this.price += this.topping;
-            this.isToppingAdded = true;
+        if(!isExtraToppingAdded) {
+            this.price += this.toppings;
+            this.isExtraToppingAdded = true;
         }
-
     }
 
     public void addTakeaway(){
         // your code goes here
-        if(!isbagAdded){
+        if(!isTakeAway) {
             this.price += 20;
-            this.isbagAdded = true;
+            this.isTakeAway = true;
         }
-
     }
 
     public String getBill(){
         // your code goes here
-        if(!isBillGenerated){
-            if(isCheeseAdded) {
+        if(!isBillCreated) {
+            if(isCheeseAdded)
                 this.bill += "Extra Cheese Added: " + this.cheese + "\n";
-            }
-            if(isToppingAdded){
-                this.bill += "Extra Toppings Added: " + this.topping + "\n";
-            }
-            if (isbagAdded){
+            if(isExtraToppingAdded)
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
+            if(isTakeAway)
                 this.bill += "Paperbag Added: 20" + "\n";
-            }
-            this.bill += "Total Price: "+getPrice()+"\n";
-            this.isBillGenerated = true;
+            this.bill += "Total Price: " + this.price + "\n";
+            this.isBillCreated = true;
         }
         return this.bill;
     }
 }
+
