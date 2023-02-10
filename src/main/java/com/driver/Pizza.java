6a0186fcffc6784972693b490279dd86ccc7dfc0
Shilpi <svvar31@gmail.com>
2023-02-10 23:08:00 +0530

    First Submit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -79,12 +79,11 @@ public class Pizza {
                 this.bill += "Extra Toppings Added: " + this.toppings + "\n";
             }
             if(isTakeAway) {
-                this.bill += "Paperbag Added: 20 " + "\n";
+                this.bill += "Paperbag Added: 20" + "\n";
             }
-            this.bill += "Total Price : " + this.price;
+            this.bill += "Total Price: " + this.price;
             this.isBillCreated = true;
-            return this.bill;
         }
-        return "";
+        return this.bill;
     }
 }
